24c757c60fa30ba1333efa5fda6c526e72765623
sh3p4rd83 <rouxromain2@gmail.com>
2024-01-11 16:31:20 -0500

    Anims bancale mais ok

--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -10,8 +10,16 @@ public class PlayerMouvement : MonoBehaviour
 
 
     [SerializeField]
-    //private Animator anim;
+    private Animator anim;
+
+    private bool isMoving = false;
+    private bool isUp = false;
+    private bool isDown = false;
+    private bool isLeft = false;
+    private bool isRight = false;
+
     // Start is called before the first frame update
+
     void Awake()
     {
     }
@@ -19,17 +27,85 @@ public class PlayerMouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //anim.SetBool("walk", false);
-        if (Input.GetAxis("Vertical") != 0)
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (vertical != 0)
         {
-            transform.position += new Vector3(0, Input.GetAxis("Vertical")) * speed * Time.deltaTime;
-            //anim.SetBool("walk", true);
+            if (!isMoving)
+            {
+                isMoving = true;
+                anim.SetBool("IsMoving", true);
+            }
+            transform.position += new Vector3(0, vertical) * speed * Time.deltaTime;
+
         }
 
-        if (Input.GetAxis("Horizontal") != 0)
+        if (horizontal != 0)
         {
-            transform.position += new Vector3(Input.GetAxis("Horizontal"), 0) * speed * Time.deltaTime;
-            //anim.SetBool("walk", true);
+            if (!isMoving)
+            {
+                isMoving = true;
+                anim.SetBool("IsMoving", true);
+            }
+            transform.position += new Vector3(horizontal, 0) * speed * Time.deltaTime;
+
+            
         }
+
+        if (horizontal + vertical == 0)
+        {
+            resetBools();
+            if (isMoving)
+            {
+                isMoving = false;
+                anim.SetBool("IsMoving", false);
+            }
+        }
+
+
+        if (vertical > 0 && !isUp)
+        {
+            resetBools();
+            isUp = true;
+            anim.SetTrigger("IsUp");
+        }
+        else if (vertical < 0 && !isDown)
+        {
+            resetBools();
+            isDown = true;
+            anim.SetTrigger("IsDown");
+        }
+        else if (horizontal < 0 && !isLeft)
+        {
+            resetBools();
+            isLeft = true;
+            anim.SetTrigger("IsLR");
+            this.gameObject.transform.localScale = new Vector3(-9f, 9f, 1f);
+        }
+        else if (horizontal > 0 && !isRight)
+        {
+            resetBools();
+            isRight = true;
+            anim.SetTrigger("IsLR");
+            this.gameObject.transform.localScale = new Vector3(9f, 9f, 1f);
+        }
+
+        setAnims();
+    }
+
+    void resetBools()
+    {
+        isUp = false;
+        isDown = false;
+        isLeft = false;
+        isRight = false;
+    }
+
+    void setAnims()
+    {
+        anim.SetBool("IsUp", isUp);
+        anim.SetBool("IsDown", isDown);
+        anim.SetBool("IsLR", isLeft || isRight);
     }
 }
