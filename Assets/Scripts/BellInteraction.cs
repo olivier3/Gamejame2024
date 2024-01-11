89733688df59009dd4ce7d35fde1ffafc7471e9e
N8Lessard <2157920@cstjean.qc.ca>
2024-01-11 14:06:30 -0500

    FEAT -- fade UI

--- Assets/Scripts/BellInteraction.cs
+++ Assets/Scripts/BellInteraction.cs
@@ -1,20 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BellInteraction : MonoBehaviour
 {
     private int bellActivationCount = 0;
     private bool inBellRange = false;
 
+    [SerializeField]
+    private Image touche;
+    [SerializeField]
+    private TMP_Text text;
     // Update is called once per frame
     void Update()
-    {
+    { 
         if (inBellRange && Input.GetKeyDown(KeyCode.E))
         {
             bellActivationCount++;
-            Debug.Log(bellActivationCount.ToString());
         }
     }
 
@@ -22,6 +27,7 @@ public class BellInteraction : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
+            StartCoroutine(FadeImage(false));
             inBellRange = true;
         }
     }
@@ -30,6 +36,7 @@ public class BellInteraction : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
+            StartCoroutine(FadeImage(true));
             inBellRange = false;
             BellValidation();
         }
@@ -45,6 +52,34 @@ public class BellInteraction : MonoBehaviour
         {
             GameLoop.Instance.AcheiveBellInteraction(false);
             bellActivationCount = 0;
+        }
+    }
+
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                touche.color = new Color(1, 1, 1, i);
+                text.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                touche.color = new Color(1, 1, 1, i);
+                text.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
         }
     }
 }
