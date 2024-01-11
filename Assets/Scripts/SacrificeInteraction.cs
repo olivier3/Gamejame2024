290561c17ea500d7f74b82e69bf99ec0c9674c62
Olivier <olivierbilodeau@outlook.com>
2024-01-11 01:02:13 -0500

    fix: refactor sacrifice script name

--- Assets/Scripts/SacrificeInteraction.cs
+++ Assets/Scripts/SacrificeInteraction.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class SacrificeInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject touche;
+
+    [SerializeField]
+    private string brocTag;
+    [SerializeField]
+    private GameObject brocObject;
+    public bool goodChoice = false;
+
+    [SerializeField]
+    private string dollTag;
+    [SerializeField]
+    private GameObject dollObject;
+
+    [SerializeField]
+    private string goatTag;
+    [SerializeField]
+    private GameObject goatObject;
+
+    [SerializeField]
+    private string mickeyTag;
+    [SerializeField]
+    private GameObject mickeyObject;
+
+    private bool choiceMade = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        touche.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.E) && !choiceMade)
+        {
+            touche.SetActive(false);
+            if (collision.collider.tag == brocTag)
+            {
+                goodChoice = true;
+                Destroy(brocObject);
+            }
+
+            if (collision.collider.tag == dollTag)
+            {
+                Destroy(dollObject);
+            }
+
+            if (collision.collider.tag == goatTag)
+            {
+                Destroy(goatObject);
+            }
+
+            if (collision.collider.tag == mickeyTag)
+            {
+                Destroy(mickeyObject);
+            }
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        
+        if (collision.collider.tag == brocTag || collision.collider.tag == goatTag
+            || collision.collider.tag == dollTag || collision.collider.tag == mickeyTag)
+        {
+            touche.SetActive(false);
+        }
     }
 }
