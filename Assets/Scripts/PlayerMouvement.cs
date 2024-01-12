0b6a6808a134610dfd2cac681c471aedc99924f4
sh3p4rd83 <rouxromain2@gmail.com>
2024-01-11 20:31:23 -0500

    Fix & fini animations

--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -13,10 +13,8 @@ public class PlayerMouvement : MonoBehaviour
     private Animator anim;
 
     private bool isMoving = false;
-    private bool isUp = false;
-    private bool isDown = false;
-    private bool isLeft = false;
-    private bool isRight = false;
+    private float offsetX = 0f;
+    private float offsetY = 0f;
 
     // Start is called before the first frame update
 
@@ -32,80 +30,36 @@ public class PlayerMouvement : MonoBehaviour
 
         if (vertical != 0)
         {
-            if (!isMoving)
-            {
-                isMoving = true;
-                anim.SetBool("IsMoving", true);
-            }
+            anim.SetFloat("MoveY", vertical);
+            offsetY = vertical * 0.01f;
+            offsetX = 0f;
             transform.position += new Vector3(0, vertical) * speed * Time.deltaTime;
-
         }
 
         if (horizontal != 0)
         {
-            if (!isMoving)
-            {
-                isMoving = true;
-                anim.SetBool("IsMoving", true);
-            }
+            anim.SetFloat("MoveX", horizontal);
+            offsetX = horizontal * 0.01f;
+            offsetY = 0f;
             transform.position += new Vector3(horizontal, 0) * speed * Time.deltaTime;
-
-            
         }
 
         if (horizontal + vertical == 0)
         {
-            resetBools();
-            if (isMoving)
-            {
-                isMoving = false;
-                anim.SetBool("IsMoving", false);
-            }
+            anim.SetFloat("MoveY", vertical + offsetY);
+            anim.SetFloat("MoveX", horizontal + offsetX);
         }
 
-
-        if (vertical > 0 && !isUp)
+        if (horizontal < 0)
         {
-            resetBools();
-            isUp = true;
-            anim.SetTrigger("IsUp");
-        }
-        else if (vertical < 0 && !isDown)
-        {
-            resetBools();
-            isDown = true;
-            anim.SetTrigger("IsDown");
-        }
-        else if (horizontal < 0 && !isLeft)
-        {
-            resetBools();
-            isLeft = true;
             anim.SetTrigger("IsLR");
             this.gameObject.transform.localScale = new Vector3(-9f, 9f, 1f);
         }
-        else if (horizontal > 0 && !isRight)
+        if (horizontal > 0)
         {
-            resetBools();
-            isRight = true;
             anim.SetTrigger("IsLR");
             this.gameObject.transform.localScale = new Vector3(9f, 9f, 1f);
         }
 
-        setAnims();
-    }
-
-    void resetBools()
-    {
-        isUp = false;
-        isDown = false;
-        isLeft = false;
-        isRight = false;
-    }
-
-    void setAnims()
-    {
-        anim.SetBool("IsUp", isUp);
-        anim.SetBool("IsDown", isDown);
-        anim.SetBool("IsLR", isLeft || isRight);
     }
 }
