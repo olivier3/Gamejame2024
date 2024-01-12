553c8c3470c9cc9ddbd3be801903452f65d77383
sh3p4rd83 <rouxromain2@gmail.com>
2024-01-12 06:18:20 -0500

    Fix musique double

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -21,6 +21,9 @@ public class MusicManager : MonoBehaviour
             {
                 Destroy(gameObject);
             }
+        } else
+        {
+            Destroy(gameObject);
         }
     }
 
